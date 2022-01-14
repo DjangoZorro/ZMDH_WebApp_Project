d96cd57754b1774a9f9864eeed2680beb2656ff1
Djelies <djelies.msk@hotmail.nl>
2022-01-14 17:43:07 +0100

    ChatFunctie
    - Added Chatfunction

--- src/ZMDH_WebApp/Controllers/HomeController.cs
+++ src/ZMDH_WebApp/Controllers/HomeController.cs
@@ -28,10 +28,16 @@ namespace ZMDH_WebApp.Controllers
             return View();
         }
 
+        public IActionResult Chat()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
     }
 }
