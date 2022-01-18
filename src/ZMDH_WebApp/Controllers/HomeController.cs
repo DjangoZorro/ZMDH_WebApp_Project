5fd6ea7179b9190d6bcf03450a3a7b09e0c7268a
Alex Sendula <58048939+AlexSendula@users.noreply.github.com>
2022-01-18 20:37:37 +0100

    Layout and translate
    Layout, home en vertaling aangepast op account/manage na.

--- src/ZMDH_WebApp/Controllers/HomeController.cs
+++ src/ZMDH_WebApp/Controllers/HomeController.cs
@@ -39,6 +39,11 @@ namespace ZMDH_WebApp.Controllers
             return View();
         }
 
+        public IActionResult Test()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
